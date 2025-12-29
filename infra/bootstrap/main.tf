2062902745bcaa1ce934d5cfc40d5adea8a5065a
AhmedMHCodeLab <ahmedmh03.work@gmail.com>
2025-12-29 17:34:07 +0300

    fix: ensure force_destroy is enabled for S3 bucket in Terraform state

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -28,7 +28,7 @@ resource "random_id" "bucket_suffix" {
 # S3 bucket for Terraform state
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "${var.state_bucket_prefix}-${random_id.bucket_suffix.hex}"
-
+  force_destroy = true
   tags = {
     Name        = "Terraform State Bucket"
     Environment = "global"
